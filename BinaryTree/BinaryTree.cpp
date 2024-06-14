8e6e573c3680d7fecd8fc0d4028e6e709111f9e3
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-06-14 11:02:15 +0700

    function to perform inorder traversal

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -70,4 +70,16 @@ public:
 			inorder(ptr->rightchild); 
 		}
 	}
+	void preorder(Node* ptr) 
+	{
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			cout << ptr->info << " "; 
+			preorder(ptr->leftchild); 
+			preorder(ptr->rightchild); 
+		}
+	}
 };
